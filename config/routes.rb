d1175d9e4bd57c285df17a38132cfe319b41da1f
David Pease <david.f.pease@gmail.com>
2020-11-16 16:06:59 -0800

    Initial push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resource :session, only: [:create, :destroy, :show]
+    resources :users, only: [:index, :update, :create, :destroy]
+  end
+
+
+  root "static_pages#root"
 end
