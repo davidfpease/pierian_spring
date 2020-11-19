a33d23d8bc84856f72dae673aa540ff2ab9c1eaf
David Pease <david.f.pease@gmail.com>
2020-11-19 07:08:58 -0800

    Create cards table

--- db/migrate/20201119142129_create_cards.rb
+++ db/migrate/20201119142129_create_cards.rb
@@ -1,11 +1,12 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-      t.integer :deck_id
-      t.text :quetion
-      t.text :answer
+      t.integer :deck_id, null: false
+      t.text :question, null: false
+      t.text :answer, null: false
 
       t.timestamps
     end
+    add_index :cards, :deck_id
   end
 end
