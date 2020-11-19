001a43a967247353ada0807857d3a78c8aea0768
David Pease <david.f.pease@gmail.com>
2020-11-18 20:22:45 -0800

    Add styling, prep for merge

--- frontend/components/greeting/banner.jsx
+++ frontend/components/greeting/banner.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const Banner = props => {
-  return (
-    <div>
-      "test"
+  return (      
+      <div className="banner">
+        <h3 className="banner-headline">Rise to
+        your challenge.</h3>
+        <h1 className="banner-sub-headline">Flashcards for <b> serious learners.</b></h1>
+        <div className="main-ctas">
+          <button className="cta-button" >
+            Find flashcards
+          </button>
+          <button className="cta-button neutral" onClick={() => props.openModal('signup')}>Make flashcards</button>
+          <button className="video-button">Watch video</button>
+        </div>
+      </div>
 
-
-
-
-
-
-    </div>
   )
 }
 
