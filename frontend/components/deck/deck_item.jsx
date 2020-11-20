ff8bb644337f68b54f10c35bafe7e52734803ae1
David Pease <david.f.pease@gmail.com>
2020-11-20 13:28:50 -0800

    Update study deck links

--- frontend/components/deck/deck_item.jsx
+++ frontend/components/deck/deck_item.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const DeckItem = ({ deck, editDeck, deleteDeck}) => {
 
   return (
-    <li >
+    <li>
       <Link to={`/study/${deck.id}`}><h3>{deck.title}</h3></Link>
       <button onClick={()=>deleteDeck(deck.id)}>Delete Deck</button>
       <button onClick={()=>editDeck(deck)}>Edit Deck</button>
