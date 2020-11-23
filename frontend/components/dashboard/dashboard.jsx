6d2bce5405159a34c2bbda7bb934e93d9c682766
David Pease <david.f.pease@gmail.com>
2020-11-22 20:20:44 -0800

    Add shell component for dashboard sidebar

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import DeckIndexContainer from '../deck/deck_index_container';
+import DashboardSidebar from './dashboard_sidebar';
 
 const Dashboard = (props) => {
   // debugger;
   return (
     <div>
-      <h1>
-        Hello from the Dashboard component
-      </h1>
-      <button className="header-button" onClick={props.logout}>Log Out</button>
+      <DashboardSidebar logout={props.logout}/>
       <DeckIndexContainer />
     </div>
   )
---
+++ frontend/components/dashboard/dashboard_sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DashboardSidebar = (props) => {
+  //profile
+  //settings
+  //add class
+  //search
+  //classes
+
+  return (
+    <div className="dashboard-sidebar">
+
+
+
+    </div>
+  )
+
+}
+
+export default DashboardSidebar;
