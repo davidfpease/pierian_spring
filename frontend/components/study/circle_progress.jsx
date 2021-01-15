1f422e3dcefa1b60348f654923b58d3ef1127e21
David Pease <david.f.pease@gmail.com>
2021-01-15 11:32:18 -0800

    Add working circular percent bar

--- frontend/components/study/circle_progress.jsx
+++ frontend/components/study/circle_progress.jsx
@@ -26,7 +26,7 @@ class CircleProgress extends Component {
     }
     this.props.originalCards.forEach(card => scores[card.score] += 1);
     let totalCards = this.props.originalCards.length;
-    let tallies = Object.keys(scores).map(key => (key * scores[key]));
+    //let tallies = Object.keys(scores).map(key => (key * scores[key]));
     //percents can't be based on how many times a score shows up.... the circle is total mastery... 
     // each color is a percent of the total mastery... 
 
@@ -36,6 +36,7 @@ class CircleProgress extends Component {
     })
     
     debugger;
+    const mastery = this.props.mastery / 100;
     const size = 500;
     const center = size / 2;
     const strokeWidth = 35.5;
@@ -47,17 +48,27 @@ class CircleProgress extends Component {
     const circleThreeStroke = '#fd0';
     const circleFourStroke = '#7fae2e';
     const circleFiveStroke = '#00a8d7';
-    const offset0 = (1-percents[0])* circumference;
-    const offset1 = (1-percents[0]-percents[1])* circumference;
-    const offset2 = (1 - percents[0] - percents[1] - percents[2]) * circumference;
-    const offset3 = (1 - percents[0] - percents[1] - percents[2] - percents[3]) * circumference;
-    const offset4 = (1 - percents[0] - percents[1] - percents[2] - percents[3] - percents[4]) * circumference;
-    const offset5 = (1 - percents[0] - percents[1] - percents[2] - percents[3] - percents[4] - percents[5]) * circumference;
+    const offset0 = 0;
+    const offset5 = (1 - mastery) * circumference;
+    const offset4 = offset5 + percents[5] * mastery * circumference;
+    const offset3 = offset4 + percents[4] * mastery * circumference;
+    const offset2 = offset3 + percents[3] * mastery * circumference;
+    const offset1 = offset2 + percents[2] * mastery * circumference;
 
     return (
       <div>
         <div className="mastery-circle">
           <svg className="svg-circles" viewBox="0 0 500 500" width="100%" height="100%">
+            <circle
+              className="svg-circle-bg"
+              stroke={circleBaseStroke}
+              cx={center}
+              cy={center}
+              r={radius}
+              strokeWidth={strokeWidth}
+              strokeDasharray={circumference}
+              strokeDashoffset={offset0}
+            />
             <circle
               className="svg-circle"
               stroke={ circleFiveStroke }
@@ -108,16 +119,7 @@ class CircleProgress extends Component {
               strokeDasharray={ circumference }
               strokeDashoffset={ offset1 }
             />
-            <circle
-              className="svg-circle-bg"
-              stroke={ circleBaseStroke }
-              cx={ center }
-              cy={ center }
-              r={ radius }
-              strokeWidth={ strokeWidth }
-              strokeDasharray={circumference}
-              strokeDashoffset={offset0}
-            />
+
             <svg className="inner-text">
               <text className="svg-circle-text" x={ center } y={ -1 * center }>
                 {this.props.mastery}%
