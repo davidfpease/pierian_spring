6114c9ab39d1cd9501b824a28acbfa240b78ce65
David Pease <david.f.pease@gmail.com>
2021-01-20 13:42:36 -0800

    Deck info container styling

--- frontend/components/study/deck_info.jsx
+++ frontend/components/study/deck_info.jsx
@@ -28,31 +28,34 @@ class DeckInfo extends React.Component {
     }
 
     return (
-      <div className="first-row">
-        <div className="branding">
-          <Link style={{ textDecoration: 'none' }} to={`/dashboard`} >
-            <div className="back-button">
-              <img id="logo" src={window.logo} />
-              <div className="back-arrow">
-                <IoIosArrowBack />
+      <div className="first-row-container">
+        <div className="first-row-deck-info">
+          <div className="branding">
+            <Link style={{ textDecoration: 'none' }} to={`/dashboard`} >
+              <div className="back-button">
+                <div className="back-arrow">
+                  <IoIosArrowBack />
+                </div>
+                <img id="logo-deck-info" src={window.logo} />
+              </div>
+            </Link>
+          </div>
+          <div className="study-mix-options-button">
+            <div className="options-menu-button">
+              <div className="options-button-deck-info">
+                <BsGearFill />
               </div>
             </div>
-          </Link>
+          </div>
         </div>
         <div className="study-mix-info">
-          <div className="study-mix-icon">
-            <img id="deck-image" src="http://localhost:3000/assets/ugs-be871e79593d46b7a66d2f4b99248d6cf90cfbc8be8559b121f1d14e81de926d.png"></img>
-            <h3 className="study-mix-name">{deck.title}</h3>
-          </div>
-        </div>
-        <div className="study-mix-options-button">
-          <div className="options-menu-button">
-            <div className="icon-button options-button gear">
-              <BsGearFill />
+            <div className="study-mix-icon">
+              <img id="deck-image-small" src="http://localhost:3000/assets/ugs-be871e79593d46b7a66d2f4b99248d6cf90cfbc8be8559b121f1d14e81de926d.png"></img>
+              <h3 className="study-mix-name">{deck.title}</h3>
             </div>
           </div>
-        </div>
       </div>
+      
     )
   }
 }
